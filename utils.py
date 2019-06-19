8d5635ab9469c98a87de900208750a78e88dc44b
Alex Miller <alex.k.miller@gmail.com>
2019-06-19 15:59:52 -0400

    Need indexes for child elements of duplicates

--- utils.py
+++ utils.py
@@ -1,31 +1,35 @@
-import os
 from lxml import etree
 import pandas as pd
 import pdb
 
-VALUE_SEPERATOR = "|"
 XPATH_SEPERATOR = "/"
 ATTRIB_SEPERATOR = "@"
 EXCLUDED_CHILDREN_TAGS = ["budget", "transaction"]
 
 
+def increment_xpath(absolute_xpath):
+    split_path = absolute_xpath.split("[")
+    indexless_path = "[".join(split_path[:-1])
+    path_index = int(split_path[-1][:-1])
+    path_index += 1
+    incremented_xpath = "{}[{}]".format(indexless_path, path_index)
+    return incremented_xpath
+
+
 def recursive_tree_traversal(element, absolute_xpath, element_dictionary):
     # Main value
     element_value = str(element.text) if element.text else ""
-    if absolute_xpath not in element_dictionary:
-        element_dictionary[absolute_xpath] = element_value
-    else:
-        element_dictionary[absolute_xpath] = VALUE_SEPERATOR.join([element_dictionary[absolute_xpath], element_value])
+    while absolute_xpath in element_dictionary:
+        absolute_xpath = increment_xpath(absolute_xpath)
+
+    element_dictionary[absolute_xpath] = element_value
 
     # Attribute values
     element_attributes = element.attrib
     for attrib_key in element_attributes.keys():
         attribute_xpath = ATTRIB_SEPERATOR.join([absolute_xpath, attrib_key])
         attribute_value = str(element_attributes[attrib_key]) if element_attributes[attrib_key] else ""
-        if attribute_xpath not in element_dictionary:
-            element_dictionary[attribute_xpath] = attribute_value
-        else:
-            element_dictionary[attribute_xpath] = VALUE_SEPERATOR.join([element_dictionary[attribute_xpath], attribute_value])
+        element_dictionary[attribute_xpath] = attribute_value
 
     # Child values
     element_children = element.getchildren()
@@ -35,7 +39,7 @@ def recursive_tree_traversal(element, absolute_xpath, element_dictionary):
         for child_elem in element_children:
             child_elem_tag = child_elem.tag
             if child_elem_tag not in EXCLUDED_CHILDREN_TAGS:
-                child_absolute_xpath = XPATH_SEPERATOR.join([absolute_xpath, child_elem_tag])
+                child_absolute_xpath = XPATH_SEPERATOR.join([absolute_xpath, child_elem_tag]) + "[0]"
                 element_dictionary = recursive_tree_traversal(child_elem, child_absolute_xpath, element_dictionary)
 
     return element_dictionary
@@ -47,45 +51,72 @@ def melt_iati(root):
     budgets_list = []
     activities = root.getchildren()
     for activity in activities:
-        activity_dict = recursive_tree_traversal(activity, "activity", {})
+        activity_dict = recursive_tree_traversal(activity, "iati-activity", {})
         activities_list.append(activity_dict)
 
         # To key transactions and budgets
-        activity_id = activity_dict["activity/iati-identifier"]
+        activity_id = activity_dict["iati-activity/iati-identifier[0]"]
 
         transactions = activity.xpath("transaction")
         for transaction in transactions:
             transaction_dict = recursive_tree_traversal(transaction, "transaction", {})
-            transaction_dict["activity/iati-identifier"] = activity_id
+            transaction_dict["iati-activity/iati-identifier[0]"] = activity_id
             transactions_list.append(transaction_dict)
 
         budgets = activity.xpath("budget")
         for budget in budgets:
             budget_dict = recursive_tree_traversal(budget, "budget", {})
-            budget_dict["activity/iati-identifier"] = activity_id
+            budget_dict["iati-activity/iati-identifier[0]"] = activity_id
             budgets_list.append(budget_dict)
 
     return (activities_list, transactions_list, budgets_list)
 
 
-def main():
-    filename = "test_data/DIPR IATI data June 2019.xml"
-    with open(filename, "r") as xmlfile:
-        original_basename, _ = os.path.splitext(os.path.basename(filename))
+def cast_iati(activities_list, transactions_list, budgets_list, iati_version="2.02"):
+    root = etree.Element('iati-activities', version=iati_version)
+    doc = etree.ElementTree(root)
+
+    activity_elems = {}
+    for activity in activities_list:
+        activity_id = activity["iati-activity/iati-identifier[0]"]
+        activity_elem = etree.SubElement(root, 'iati-activity')
+        pdb.set_trace()
+    # pageElement = etree.SubElement(root, 'Country', name='Germany', Code='DE', Storage='Basic')
+
+    return doc
+
+
+def xml_to_csv(xml_filename="test_data/DIPR IATI data June 2019.xml", a_filename="test_data/activities.csv", t_filename="test_data/transactions.csv", b_filename="test_data/budgets.csv"):
+    with open(xml_filename, "r") as xmlfile:
         tree = etree.parse(xmlfile)
         root = tree.getroot()
 
         activities, transactions, budgets = melt_iati(root)
         a_df = pd.DataFrame(activities)
         a_df = a_df.reindex(sorted(a_df.columns), axis=1)
-        a_df.to_csv("test_data/activities.csv", index=False)
+        a_df.to_csv(a_filename, index=False)
         t_df = pd.DataFrame(transactions)
         t_df = t_df.reindex(sorted(t_df.columns), axis=1)
-        t_df.to_csv("test_data/transactions.csv", index=False)
+        t_df.to_csv(t_filename, index=False)
         b_df = pd.DataFrame(budgets)
         b_df = b_df.reindex(sorted(b_df.columns), axis=1)
-        b_df.to_csv("test_data/budgets.csv", index=False)
+        b_df.to_csv(b_filename, index=False)
+
+
+def csv_to_xml(a_filename="test_data/activities.csv", t_filename="test_data/transactions.csv", b_filename="test_data/budgets.csv", xml_filename="test_data/output.xml"):
+    a_df = pd.read_csv(a_filename).fillna("")
+    t_df = pd.read_csv(t_filename).fillna("")
+    b_df = pd.read_csv(b_filename).fillna("")
+    activities = a_df.to_dict("records")
+    transactions = t_df.to_dict("records")
+    budgets = b_df.to_dict("records")
+
+    doc = cast_iati(activities, transactions, budgets)
+
+    with open(xml_filename, "w") as xmlfile:
+        doc.write(xmlfile, encoding="utf-8")
 
 
 if __name__ == "__main__":
-    main()
+    xml_to_csv()
+    csv_to_xml()
